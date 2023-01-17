3f5ce2f9f3ae421605ecc40e4b3e232b5cdf2c32
Fene-87 <fenekayas87@gmail.com>
2023-01-17 23:02:10 +0300

    Set up project with Webpack, Create to-do-list structure

--- src/index.js
+++ src/index.js
@@ -3,10 +3,47 @@ import './style.css';
 
 function component() {
     const element = document.createElement('div');
+    element.classList.add('element-list');
+    element.innerHTML = `
+      <div class="head-div">
+        <div class="first-row"><h3>Today's To Do</h3> <span><i class="fa-sharp fa-solid fa-arrows-spin"></i></span> </div>
+        <div class="first-row"><input type="text" class="book-input" id="book-title" placeholder="Add to your list..."/> <span><i class="fa-solid fa-plus"></i></span> </div>
+      </div>
+    `
+    const tasks = [
+      {
+        description: 'Wash the dishes',
+        completed: false,
+        index: 0,
+      },
+
+      {
+        description: 'Complete to do list project',
+        completed: false,
+        index: 1,
+      },
+
+    ]
   
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    tasks.forEach((task) => {
+      const row = document.createElement('div');
+      row.classList.add('task-row');
+      row.innerHTML = `
+        <div class="task-element">
+          <div class="task-description">
+            <input type="checkbox"/> 
+            <div class="task-text">${task.description}</div> 
+          </div> 
+          <div class="task-icon"><i class="fa-solid fa-ellipsis-vertical"></i></div>
+        </div>
+      `
+      element.appendChild(row);
+    })
+
+    const clearButton = document.createElement('button');
+    clearButton.textContent = 'Clear all completed';
+    clearButton.classList.add('clear-button');
+    element.appendChild(clearButton);
   
     return element;
   }
